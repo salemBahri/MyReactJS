8c9f49a0cd6bb6891e0424b70277aeece3038ecf
Salem Bahri <bahrisalem5@gmail.com>
2024-11-07 22:34:37 +0100

    show and gide component

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,21 +1,29 @@
+import {useState} from "react";
 import FirstComponent from "./FirstComponent";
 import Notification from "./Notification";
 import Button from "./Button";
 
 const Main=(e)=>{
-
     const handle=()=>{
         console.log("clicked");
-        
+        isShown(!shown)
+         
     }
-
+    const [shown,isShown]=useState(false);
+    const title=shown ? "Hide" : "Show"
     return(
         <main className="main">
-            <Button title="show" onClick={handle}/>
-            <Notification text="Hi I am new notification"/>
+            <Button title={title} onClick={handle}/>
+            {
+                shown
+                &&
+                <Notification text="Hi I am new notification"/>
+            }
+            
             <h1>Main Component</h1>
             <FirstComponent />
         </main>
     )
 }
 export default Main;
+
